5c5dc41e1c3be27dedd613d9fa5ff75589898410
calebandria <kalebaandriamanaja@gmail.com>
2024-03-05 18:02:04 +0300

    problem with react hook form, 'errors' not undefined

--- src/routes/sign-up/sign-up.component.jsx
+++ src/routes/sign-up/sign-up.component.jsx
@@ -1,41 +1,67 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
 import './sign-up.styles.scss'
-import {supabase} from '../../utils/supabase/supabase.utils'
-import {ReactComponent as GithubLogo} from '../../assets/github-icon.svg'
-import {ReactComponent as GoogleLogo} from '../../assets/google_icon.svg'
-import {ReactComponent as FacebookLogo} from '../../assets/facebook_log.svg'
+import { supabase } from '../../utils/supabase/supabase.utils'
+import { ReactComponent as GithubLogo } from '../../assets/github-icon.svg'
+import { ReactComponent as GoogleLogo } from '../../assets/google_icon.svg'
+import { ReactComponent as FacebookLogo } from '../../assets/facebook_log.svg'
 
-const SignUp = ()=>{
-   const login_github  = async() =>{
-    await supabase.auth.signInWithOAuth({
-        provider:"github"
-    });
-    } 
 
-    const login_google  = async() =>{
+const SignUp = () => {
+    const { register, handleSubmit, formstate: { errors } } = useForm();
+
+    const onSubmit = (data) => {
+        console.log('Form submitted:', data);
+    }
+
+
+
+    const login_github = async () => {
         await supabase.auth.signInWithOAuth({
-            provider:"google"
+            provider: "github"
         });
     }
-    const login_facebook  = async() =>{
+
+    const login_google = async () => {
         await supabase.auth.signInWithOAuth({
-            provider:"facebook"
+            provider: "google"
         });
     }
-    
+    const login_facebook = async () => {
+        await supabase.auth.signInWithOAuth({
+            provider: "facebook"
+        });
+    }
 
-    
-    return(
+
+
+    return (
         <div className='sign_up'>
-            <form action="">
-                <input type="text" placeholder='First name'/>
-                <input type="text" placeholder='Family name'/>
-                <input type="email" placeholder='Email address'/>
-                <input type="password" placeholder='Password'/>
-                <input type="password" placeholder='Confirm password'/>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <input type="text" id="username" placeholder='Username'  {...register('username', { required: 'Username is required' })} />
+                    {errors.username && <span>{errors.username.message}</span>}
+                </div>
+                <div>
+                    <input type="email" id="email" placeholder='Email' {...register('email', { required: 'Email is required' })} />
+                    {errors.email && <span>{errors.email.message}</span>}
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" id="password" placeholder='Password' {...register('password', { required: 'Password is required' })} />
+                    {errors.password && <span>{errors.password.message}</span>}
+                </div>
+                <div>
+                    <label for="confirmPassword">Confirm Password</label>
+                    <input type="password" id="confirmPassword" placeholder='Confirm Password'  {...register('confirmPassword', { required: 'Confirm Password is required' })} />
+                    {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>}
+                </div>
+                <button type="submit">Sign Up</button>
             </form>
-            <GoogleLogo className='google_logo' onClick={login_google}/>
-            <FacebookLogo className='facebook_logo' onClick={login_facebook}/>
-            <GithubLogo className='github_logo' onClick={login_github}/>
+            <GoogleLogo className='google_logo' onClick={login_google} />
+            <FacebookLogo className='facebook_logo' onClick={login_facebook} />
+            <GithubLogo className='github_logo' onClick={login_github} />
         </div>
     )
 }
