b5c2450efa46b7f4caaffec8b80a40857a9aac91
calebandria <kalebaandriamanaja@gmail.com>
2024-03-10 16:05:32 +0300

    mise en place fait

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes,Route} from 'react-router-dom'
+import { useEffect, useState } from 'react';
 import Home from "./routes/home/home.component";
 import Navigation from './routes/navigation/navigation.component';
 import Themes from './routes/themes/themes.component';
 import SignUp from './routes/sign-up/sign-up.component';
 import LogIn from './routes/login/login.component';
+
 const Questions = ()=>{
   return(
       <h1>I am Questions</h1>
@@ -15,11 +17,24 @@ const Answers = ()=>{
   )
 }
 const App = () => {
+  const [token, setToken] = useState(false);
+
+  if(token){
+    sessionStorage.setItem('token',JSON.stringify(token));
+  }
+
+  /* maintaining the subscription over all the application */
+  useEffect(()=>{
+    if(sessionStorage.getItem('token'))
+      setToken(sessionStorage.getItem('token'))
+  },[])
+
+
   return (
     <Routes>
       <Route path='/' element= {<Navigation />}>
-      <Route index element= {<LogIn />}/>
-        <Route path='homepage' element= {<Home />}/>
+      <Route index element= {<LogIn setToken={setToken} />}/>
+        {token ?<Route path='homepage' element= {<Home />}/>:""}
         <Route path='themes' element ={<Themes/>} />
         <Route path='questions' element ={<Questions/>} />
         <Route path='answers' element ={<Answers/>} />
--- src/routes/login/login.component.jsx
+++ src/routes/login/login.component.jsx
@@ -5,23 +5,25 @@ import './login.styles.scss'
 import { useNavigate } from 'react-router-dom'
 
 
-const LogIn = ()=>{
-    const { register, handleSubmit, formState: { errors }, watch , reset} = useForm();
+const LogIn = ({setToken})=>{
+    const { register, handleSubmit, formState: { errors }} = useForm();
+
     let navigate = useNavigate();
+
     const onSubmit = async(formData) =>{
         try {
             const { data, error } = await supabase.auth.signInWithPassword({
             email: formData.email,
-            password: formData.password,
-            
-            
+            password: formData.password, 
           })
           if(error) throw(error)
+          console.log(data)
+          setToken(data)
+          navigate("/homepage")
 
         } catch (error) {
             alert(error)
         }
-        
     }
 
     return(
@@ -29,8 +31,8 @@ const LogIn = ()=>{
             <form onSubmit={handleSubmit(onSubmit)} className='mail_signup'>
                 <h3>LOG IN</h3>
                 <div>
-                    <input type="email" id="email" placeholder='email'  {...register('email', { required: 'email is required' })} />
-                    {<errors className="email"></errors> && <span><br />{errors.email.message}</span>}
+                    <input type="email" id="email" placeholder='email'  {...register('email', { required: 'Email is required' })} />
+                    {errors.email && <span><br/>{errors.email.message}</span>}
                 </div>
                 <div>
                     <input type="password" id="password" placeholder='Password'  {...register('password', { required: 'Password is required' })} />
