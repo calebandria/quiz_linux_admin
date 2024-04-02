c0b83fb02390354f590a674d11262fd8d77e80c9
calebandria <kalebaandriamanaja@gmail.com>
2024-04-02 12:47:29 +0300

    ajout du component edit-question

---
+++ src/components/edit-question/edit-question.component.jsx
@@ -0,0 +1,131 @@
+import './edit-question.styles.scss'
+import Icon from '@mui/material/Icon'
+import CloseIcon from '@mui/icons-material/Close';
+import { supabase } from '../../utils/supabase/supabase.utils'
+import { useContext, useEffect } from 'react'
+
+import { Controller, useForm } from 'react-hook-form'
+import { DevTool } from '@hookform/devtools'
+import { FormControl, InputLabel, MenuItem, Select, TextField, Button } from '@mui/material';
+import { ThemesContext } from '../../contexts/themes.context';
+
+
+const EditQuestion = ({ handleClickEdit, label, id, idTheme, setQuestions }) => {
+
+    const { themes } = useContext(ThemesContext)
+    const form = useForm({
+        defaultValues: {
+            question: label,
+            id_theme: idTheme
+        }
+    });
+
+
+    const { control, handleSubmit, reset, formState } = form;
+    const { errors, isSubmitSuccessful } = formState;
+
+    const onSubmit = async (event) => {
+        try {
+            const { data, error } = await supabase
+                .from('question')
+                .update([
+                    { question: event.question, id_theme: event.id_theme },
+                ])
+                .eq('id_question', id)
+                .select()
+
+            if (error) {
+                throw(error)
+            }
+            
+
+            alert("Question updated successfully: ", data)
+        } catch (error) {
+            alert("An error occured", error.message)
+        }
+
+    }
+
+    useEffect(() => {
+        if (isSubmitSuccessful) {
+            reset();
+            const fetchQuestion = async () => {
+                let { data: question, error } = await supabase
+                    .from('question')
+                    .select('*')
+
+                if (error) {
+                    alert(error)
+                }
+                else setQuestions(question)
+            }
+            fetchQuestion()
+        }
+    }, [isSubmitSuccessful, reset, setQuestions])
+
+    return (
+        <div className="create-question" /* style={{ display: activeCrea ? "block" : "none" }} */ onSubmit={handleSubmit(onSubmit)}>
+            <form className='add-question' method="post" noValidate>
+                <h2>Add a question</h2>
+                <Controller
+                    name="question"
+                    control={control}
+                    defaultValue=''
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            className='question'
+                            label="Question"
+                            fullWidth
+                            required
+                            sx={{
+                                '& .MuiOutlinedInput-root': {
+                                  backgroundColor: 'white', // Set the background color to white
+                                  '& fieldset': {
+                                    borderColor: 'primary.main', // Adjust the border color if needed
+                                  },
+                                  '&:hover fieldset': {
+                                    borderColor: 'primary.main', // Adjust the border color on hover if needed
+                                  },
+                                  '&.Mui-focused fieldset': {
+                                    borderColor: 'primary.main', // Adjust the border color when focused if needed
+                                  },
+                                },
+                              }}
+                            
+                        />
+                    )}
+                />
+                <FormControl variant='filled'required fullWidth>
+                    <InputLabel id="demo-simple-select-required-label">Theme</InputLabel>
+                    <Controller
+                        name="id_theme"
+                        control={control}
+                        defaultValue=''
+                        render={({ field }) => (
+
+                            <Select
+                                {...field}
+                                labelId='demo-simple-select-required-label'
+                                id="demo-simple-select-required"
+                                fullWidth
+
+                            >
+                                {themes.map((theme, id) => (
+                                    <MenuItem key={id} value={theme.id_theme}>{theme.theme}</MenuItem>
+                                ))}
+                            </Select>
+                        )}
+                    />
+                </FormControl>
+                <p className="error">{errors.label?.message}</p>
+                <Button type="submit" value="edit">Edit</Button>
+            </form>
+            <DevTool control={control} />
+            <div className="close-button" onClick={handleClickEdit}>
+                <Icon sx={{ color: '#FFA629' }}><CloseIcon /></Icon>
+            </div>
+        </div>
+    )
+}
+export default EditQuestion;
--- src/routes/questions/questions.component.jsx
+++ src/routes/questions/questions.component.jsx
@@ -7,12 +7,16 @@ import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteQuestion from '../../components/delete-question/delete-question.component';
+import EditQuestion from '../../components/edit-question/edit-question.component';
 
 const Questions = () => {
     const [activeCrea, setActiveCrea] = useState(false);
     const { questions, setQuestions } = useContext(QuestionsContext);
     const [id, setId] = useState(0);
     const [activeDel, setActiveDel] = useState(false);
+    const [activeEdit, setActiveEdit] = useState(false);
+    const [label, setLabel] = useState("");
+    const [idTheme, setIdTheme] = useState(0);
 
     const mesDel = "Are you sure to delete this question and all the anwsers with it?"
 
@@ -28,6 +32,14 @@ const Questions = () => {
         else setActiveDel(true)
     }
 
+    const handleClickEdit = () => {
+        console.log("clickedfqsdf")
+        if (activeEdit)
+          setActiveEdit(false)
+        else setActiveEdit(true)
+      }
+    
+
     return (
         <div className="container">
             <div className="questions">
@@ -42,9 +54,10 @@ const Questions = () => {
                                     <li>{question.id_theme}</li>
                                     <li>
                                         <Icon onClick={() => {
-                                            /* handleClickEdit()
-                                            setLabel(theme.theme)
-                                            setId(theme.id_theme) */
+                                            setId(question.id_question)
+                                            handleClickEdit()
+                                            setLabel(question.question)
+                                            setIdTheme(question.id_theme)
                                         }}
                                             className='edit'
                                             sx={{ color: '#1e1e1e' }} >
@@ -71,6 +84,7 @@ const Questions = () => {
             </div>
             {activeCrea && <CreateQuestion activeCrea={activeCrea} handleClickCrea={handleClickCrea} setQuestions={setQuestions} />}
             {activeDel && <DeleteQuestion messageDelete={mesDel} id={id} handleClickDele={handleClickDel} setQuestions={setQuestions}/>}
+            {activeEdit && <EditQuestion handleClickEdit={handleClickEdit} label={label} id={id}  idTheme={idTheme} setQuestions={setQuestions}/>}
             <div className='overlay-visible' style={{ display: (activeCrea | activeDel) ? "block" : "none" }}></div>
             
         </div> 
